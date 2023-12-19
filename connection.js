3086dd7d2f3fdb287e025dcada347e9353286fc8
44m1r <151672453+44m1r@users.noreply.github.com>
2023-12-19 22:38:04 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 import sql from "mysql"
 
 const connection = sql.createConnection({
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12671606',
-    password: '2LIdDq6ukz',
-    database: 'sql12671606',
+    host: 'mysql-5abcd1d-codingonly41-2cf1.a.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_vVm81iIwC5wWV97y27i',
+    database: 'defaultdb',
   });
   
   // Connect to MySQL
